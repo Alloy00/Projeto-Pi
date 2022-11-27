f0f38c8165b12a86ba940a3052fd0bfe1e3f4b5c
marconekc <kaikeds8@gmail.com>
2022-11-27 15:20:29 -0300

    Tela com botões linkados da trilha colab, atualizações no js e telas

--- PITelasGerente/script.js
+++ PITelasGerente/script.js
@@ -26,3 +26,32 @@ function showButton()
         btn.style.display = 'block';
     }
 }
+
+
+var yourbuttons = document.getElementsByClassName('btncolab');
+var idThis;
+const id1 = "colab1";
+const id2 = "colab2";
+const id3 = "colab3";
+var highlightIni = 0;
+var idIni = document.getElementById('colab2');
+for (var i = yourbuttons.length - 1; i >= 0; i--) {
+    var currentbtn;
+    yourbuttons[i].onclick=function(){
+        if (highlightIni == 0)
+        {
+            idIni.classList.remove("highlight");
+            highlightIni = 1;
+        }
+         if(currentbtn){
+             currentbtn.classList.remove("highlight");
+          }
+          this.classList.add("highlight");
+          currentbtn=this;
+          idThis = this.id;
+          if (idThis === id1){MostraElemento('divcolab1'); EscondeElemento('divcolab2'); EscondeElemento('divcolab3');}
+          if (idThis === id2){MostraElemento('divcolab2'); EscondeElemento('divcolab1'); EscondeElemento('divcolab3');}
+          if (idThis === id3){MostraElemento('divcolab3'); EscondeElemento('divcolab1'); EscondeElemento('divcolab2');}
+    }
+
+};
